3e35456111123d05aeabf241d0f5765c58f53813
ainarashost1-droid <ainarashost1@gmail.com>
2026-05-31 19:13:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
 const CACHE = 'accounts-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = [
+  '/accounts-manager/',
+  '/accounts-manager/index.html',
+  '/accounts-manager/manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
